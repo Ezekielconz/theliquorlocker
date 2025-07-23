29afc1f8160cd58e91a60e7902388f3780d90574
Ezekiel <zek.bro@hotmail.com>
2025-07-23 19:07:01 +1200

    responsive footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,37 +8,34 @@ export default function Footer({ logoUrl, logoAlt = 'The Liquor Locker' }) {
   return (
     <footer className={styles.footer}>
       <div className={styles.column}>
-        <div className={styles.logo}>
-          <Link href="/" className={styles.logoLink}>
-            {logoUrl ? (
-              <Image
-                src={logoUrl}
-                alt={logoAlt}
-                width={120}
-                height={40}
-                priority
-              />
-            ) : (
-              <span>{logoAlt}</span>
-            )}
-          </Link>
-        </div>
+        <Link href="/" className={styles.logo}>
+          {logoUrl ? (
+            <Image src={logoUrl} alt={logoAlt} width={120} height={40} priority />
+          ) : (
+            <span>{logoAlt}</span>
+          )}
+        </Link>
       </div>
 
       <div className={styles.column}>
         <div className={styles.details}>
-          <a href="mailto:Ben@thespiritsnetwork.co.nz">
-            Ben@thespiritsnetwork.co.nz
-          </a>
-          <a href="tel:+64276256220">+64 27 625 6220</a>
-          <span>Auckland, New Zealand</span>
+          <a href="mailto:Ben@thespiritsnetwork.co.nz">Ben@thespiritsnetwork.co.nz</a>
+          <a href="tel:+64276256220">+64&nbsp;27&nbsp;625&nbsp;6220</a>
+          <span>Auckland, New&nbsp;Zealand</span>
         </div>
       </div>
 
       <div className={styles.column}>
         <div className={styles.myDetails}>
-          <span>Created & Designed By</span>
-          <strong>Ezekiel</strong>
+          <span>Created&nbsp;&amp;&nbsp;Designed&nbsp;By</span>
+          <a
+            href="https://ezekiel.co.nz/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.creatorLink}
+          >
+            Ezekiel
+          </a>
         </div>
       </div>
     </footer>
